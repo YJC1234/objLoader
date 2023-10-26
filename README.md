893fba1f7815a29f337f4de25b04c67816bce2c0
Yuan Jin cheng <1411488659@qq.com>
2023-10-26 12:04:20 +0800

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # objLoader
-obj文件解析器,分别使用普通阻塞，io_uring，io_uring+协程, io_uring+线程池解析
+obj文件解析器,分别使用普通阻塞，io_uring，io_uring+协程, io_uring+协程+线程池解析  
+仅作为个人练习
---
+++ build/CMakeFiles/obj_loader.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/obj_loader.dir/main.cpp.o"
+  "CMakeFiles/obj_loader.dir/main.cpp.o.d"
+  "CMakeFiles/obj_loader.dir/tiny_obj_loader.cpp.o"
+  "CMakeFiles/obj_loader.dir/tiny_obj_loader.cpp.o.d"
+  "obj_loader"
+  "obj_loader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/obj_loader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
